5a56fac75010964128bc81e54f58d9e7e11cc67d
amalazza <dinaazza8@gmail.com>
2020-05-18 08:52:07 +0700

    excel buat import data siswa

--- anggota/View/detail.php
+++ anggota/View/detail.php
@@ -171,10 +171,10 @@
                             <p><span>Stok </span>: <?=$this->oBuku->stok?></p>
                           </div>
                           <div>
-                            <p><span>Bentuk Buku</span>: <span id="jenis_katalog"><?=$this->oBuku->jenis_katalog?></span></p>
+                            <p><span>Bentuk Buku</span>: <span id="jenis_katalog" style="display: inline;"><?=$this->oBuku->jenis_katalog?></span></p>
                           </div>
-                          <div onclick="window.location='<?=ROOT_URL?>?p=buku&amp;a=perpanjangan&amp;id=<?=$this->oBuku->no_katalog?>'" class="btn btn-primary" style="background-color: #2c3e50; color: white; border-color: white;">Perpanjangan
-                          </div>
+                          <!-- <div onclick="window.location='<?=ROOT_URL?>?p=buku&amp;a=perpanjangan&amp;id=<?=$this->oBuku->no_katalog?>'" class="btn btn-primary" style="background-color: #2c3e50; color: white; border-color: white;">Perpanjangan
+                          </div> -->
                         </div>
                     </div>
                   </div>
