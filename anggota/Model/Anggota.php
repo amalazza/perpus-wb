4d79da98ca7df60ecd66b689f2b66b0e06916e8b
amalazza <dinaazza8@gmail.com>
2020-06-28 04:10:24 +0700

    tab belum dinilai dan sudah dinilai

--- anggota/Model/Anggota.php
+++ anggota/Model/Anggota.php
@@ -129,7 +129,7 @@ public function addAngLog(array $aLog)
 
     public function getBelumDinilaiById($iId)
     {
-        $oStmt = $this->oDb->prepare('SELECT * FROM peminjaman p inner join katalog k on k.no_katalog = p.no_katalog inner join anggota a on a.no_anggota = p.no_anggota INNER JOIN klasifikasi KL ON KL.no_klasifikasi = K.no_klasifikasi INNER JOIN koleksi KO ON KO.no_koleksi = K.no_koleksi WHERE p.status="kembali" and p.rate="not yet" and p.no_anggota = :no_anggota');
+        $oStmt = $this->oDb->prepare('SELECT * FROM peminjaman p inner join rating r on r.no_katalog = p.no_katalog inner join katalog k on k.no_katalog = p.no_katalog inner join anggota a on a.no_anggota = p.no_anggota INNER JOIN klasifikasi KL ON KL.no_klasifikasi = K.no_klasifikasi INNER JOIN koleksi KO ON KO.no_koleksi = K.no_koleksi WHERE p.status="kembali" and r.rate="not yet" and p.no_anggota = :no_anggota');
         $oStmt->bindParam(':no_anggota', $iId, \PDO::PARAM_INT);
         $oStmt->execute();
         return $oStmt->fetchAll(\PDO::FETCH_OBJ);
@@ -137,7 +137,7 @@ public function addAngLog(array $aLog)
 
     public function getSudahDinilaiById($iId)
     {
-        $oStmt = $this->oDb->prepare('SELECT * FROM peminjaman p inner join katalog k on k.no_katalog = p.no_katalog inner join anggota a on a.no_anggota = p.no_anggota INNER JOIN klasifikasi KL ON KL.no_klasifikasi = K.no_klasifikasi INNER JOIN koleksi KO ON KO.no_koleksi = K.no_koleksi WHERE p.status="kembali" and p.rate="yes" and p.no_anggota = :no_anggota');
+        $oStmt = $this->oDb->prepare('SELECT * FROM peminjaman p inner join rating r on r.no_katalog = p.no_katalog inner join katalog k on k.no_katalog = p.no_katalog inner join anggota a on a.no_anggota = p.no_anggota INNER JOIN klasifikasi KL ON KL.no_klasifikasi = K.no_klasifikasi INNER JOIN koleksi KO ON KO.no_koleksi = K.no_koleksi WHERE p.status="kembali" and r.rate="yes" and p.no_anggota = :no_anggota');
         $oStmt->bindParam(':no_anggota', $iId, \PDO::PARAM_INT);
         $oStmt->execute();
         return $oStmt->fetchAll(\PDO::FETCH_OBJ);
